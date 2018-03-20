512d9eb9510b0c2b63b4340a21bca803da8ba7f7
Darkness-Storm <golubinda@yandex.ru>
2018-03-20 13:03:06 +0300

    added view for export xls

--- urls.py
+++ urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'parser_log'
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^export/xls$', views.export_xls, name='export_xls'),
     ]
--- views.py
+++ views.py
@@ -1,11 +1,14 @@
 from collections import Counter, OrderedDict
+import datetime
 
 from django.db.models import Sum, Q
+from django.http import HttpResponse
 from django.views.generic import ListView
+
+import xlwt
 
 from .models import ApacheLog
 from .forms import SearchForm
-
 
 
 class IndexView(ListView):
@@ -14,34 +17,37 @@ class IndexView(ListView):
     model = ApacheLog
     context_object_name = 'log'
     template_name = 'parser_log/index.html'
+    criteria = None
 
     def get_queryset(self):
-        criteria = self.request.GET.get('search')
-        if criteria:
-            return ApacheLog.objects.filter(
-                Q(ip__icontains=criteria)
-                | Q(date__icontains=criteria)
-                | Q(method__icontains=criteria)
-                | Q(url__icontains=criteria))
+        self.criteria = self.request.GET.get('search')
+        if self.criteria:
+            queryset_log = ApacheLog.objects.filter(
+                Q(ip__icontains=self.criteria)
+                | Q(date__icontains=self.criteria)
+                | Q(method__icontains=self.criteria)
+                | Q(url__icontains=self.criteria))
         else:
-             return ApacheLog.objects.all()
+            queryset_log = ApacheLog.objects.all()
+        return queryset_log
 
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
+        context['criteria'] = self.criteria
         # queryset без пагинациии
         query_log = self.get_queryset()
 
         # Сначала я неправильно понял задачу и выводил итоги по странице
-        #query_log = context['log']
+        # query_log = context['log']
 
-        #количество записей
+        # количество записей
         context['count_records'] = query_log.count()
 
         # количество переданных байт
         sum_resp = query_log.aggregate(Sum('resp_size'))
         context['sum_responce'] = sum_resp
 
-        #уникальные ip
+        # уникальные ip
         list_ip = query_log.values_list('ip')
         sum_unique_ip = len(set(list_ip))
         context['sum_unique_ip'] = sum_unique_ip
@@ -50,12 +56,65 @@ class IndexView(ListView):
         unique_ip = [(k[0], v) for k, v in new_d.items()][0:11]
         context['unique_ip'] = unique_ip
 
-        #информация по http-методам
+        # информация по http-методам
         sum_method = [(k[0], v) for k, v in Counter(query_log.values_list('method')).items()]
         context['sum_method'] = sum_method
 
-        #поисковая форма
+        # поисковая форма
         form = SearchForm()
         context['form'] = form
         return context
+
+
+def export_xls(request):
+    response = HttpResponse(content_type='application/ms-excel')
+    response['Content-Disposition'] = 'attachment; filename="log.xls"'
+    criteria = request.GET.get('criteria')
+    # если был использован поиск
+    if criteria:
+        queryset_log = ApacheLog.objects.filter(
+            Q(ip__icontains=criteria)
+            | Q(date__icontains=criteria)
+            | Q(method__icontains=criteria)
+            | Q(url__icontains=criteria))
+
+    else:
+        queryset_log = ApacheLog.objects.all()
+
+    wb = xlwt.Workbook(encoding='utf-8')
+    ws = wb.add_sheet('Log')
+
+    row_num = 0
+
+    font_style = xlwt.XFStyle()
+    font_style.font.bold = True
+
+    columns = ['id', 'IP', 'Date', 'Method', 'URL', 'Response code', 'Response size']
+
+    for col_num in range(len(columns)):
+        ws.write(row_num, col_num, columns[col_num], font_style)
+
+    font_style = xlwt.XFStyle()
+
+    for row in queryset_log:
+        # нужно в цикле по списку полей, только траблы с datetime Python
+        row_num += 1
+        col = 0
+        ws.write(row_num, col, row.pk, font_style)
+        col += 1
+        ws.write(row_num, col, row.ip, font_style)
+        col += 1
+        date = datetime.datetime.strftime(row.date, '%a, %d %b %Y %H:%M:%S')
+        ws.write(row_num, col, date, font_style)
+        col += 1
+        ws.write(row_num, col, row.method, font_style)
+        col += 1
+        ws.write(row_num, col, row.url, font_style)
+        col += 1
+        ws.write(row_num, col, row.id_resp, font_style)
+        col += 1
+        ws.write(row_num, col, row.resp_size, font_style)
+
+    wb.save(response)
+    return response
 
