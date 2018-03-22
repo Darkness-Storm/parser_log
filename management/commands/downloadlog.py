8e6cb8532191d43b9b36880649f2fee5a1660375
Darkness-Storm <golubinda@yandex.ru>
2018-03-22 16:17:55 +0300

    parsing with RegExp

--- management/commands/downloadlog.py
+++ management/commands/downloadlog.py
@@ -1,6 +1,9 @@
+import logging
+
 import requests
 import datetime
 import os.path
+import re
 
 from django.core.management.base import BaseCommand, CommandError
 from django.conf import settings
@@ -22,20 +25,32 @@ MONTH_NAME = {
     'Dec': 12,
 }
 
+# lexeme types
+WSP, QUOTED_STRING, DATE, RAW, NO_DATA, IP = range(6) # ENUM
+
+RULES = [
+    ('\s+', WSP),
+    ('-|"-"', NO_DATA),
+    ('[0-9]{1,3}(\.[0-9]{1,3}){3}', IP),
+    ('"([^"]+)"', QUOTED_STRING),
+    ('\[([^\]]+)\]', DATE),
+    ('([^\s]+)', RAW),
+]
+
 
 class Command(BaseCommand):
     help = """Accepts a link to a log file of a certain format, downloads it, 
     parses it and writes it to DB."""
     size_file = 0
-    path_file = 'access.log'
+    path_file = 'access1.log'
 
     def add_arguments(self, parser):
         parser.add_argument('url', nargs='*', type=str)
 
     def handle(self, *args, **options):
         url = options.get('url')[0]
-        self.download(url)
-        self.parse_log()
+#        self.download(url)
+        self.parse_log_re()
 
     def download(self, url):
         r = requests.get(url, stream=True)
@@ -133,3 +148,63 @@ class Command(BaseCommand):
         full_date = datetime.datetime(year, month, day, hour, min, sec)
         return full_date
 
+    def parse_log_re(self):
+        with open(self.path_file, 'r') as file:
+            self.stdout.write('Start parsing and writing to DB.')
+            #количество обработанных строк
+            count_str = 0
+            #размер обработанных строк в байтах
+            size_record = 0
+            self.size_file = os.path.getsize(self.path_file)
+            # список для для массового сохранения объектов ApacheLog
+            list_log = []
+            lexer = Lexer(RULES)
+
+            for s in file:
+                count_str += 1
+                size_record += len(s.encode('utf-8'))
+                try:
+                    tokens = lexer(s)
+                except Exception:
+                    logging.exception("Error in line '%s'", s)
+                    continue  # пропускаем битые строки
+                #print(tokens)
+
+                for re_match, token_type in tokens:
+                    if token_type == WSP:
+                        continue  # пробелы игнорируем
+                    elif token_type == NO_DATA:
+                        value = None  # NO_DATA заменяем на None
+                    elif token_type == RAW:
+                        value = re_match.group(1)  # MatchObject.group(i) возвращает i-ую заключённую в круглые скобки группу
+                    elif token_type == QUOTED_STRING:
+                        value = re_match.group(1)  # снимаем экранирование с заэкранированных кавычек
+                    elif token_type == DATE:
+                        value = datetime.datetime.strptime(re_match.group(1),
+                                                           "%d/%b/%Y:%H:%M:%S %z")  # парсим дату
+                    elif token_type == IP:
+                        value = re_match.group(0)
+                    else:
+                        raise SyntaxError("Unknown token", token_type, re_match)
+                    print(value)
+
+
+#x = datetime.datetime.strptime('13/Dec/2015:14:29:19 +0100','%d/%b/%Y:%H:%M:%S %z')
+
+def Lexer(rules):
+    # предварительно компилируем регулярные выражения для ускорения работы
+    prepared = [(re.compile(regexp), token_type) for regexp, token_type in rules]
+
+    def lex(line):
+        ll = len(line) # длина строки лога - чтобы знать, когда остановиться
+        i = 0          # текущая позиция анализатора
+        while i < ll:
+            for pattern, token_type in prepared:  # пробуем регулярные выражения по очереди
+                match = pattern.match(line, i)    # проверяем соответствует ли регулярное выражение строке с позиции i
+                if match is None:                 # если нет - пробуем следующую регулярку
+                    continue
+                i = match.end()                   # передвигаем позицию анализатора до индекса, соответствующего концу совпадения
+                yield (match, token_type)         # возвращаем найденный токен
+                break                             # начинаем анализировать остаток строки с новым значением сдвига i
+            # по хорошему, в этом месте нужно кидать ошибку SyntaxError(line, i) в случае, если ни один из шаблонов не совпал
+    return lex
---
+++ management/commands/test1.py
@@ -0,0 +1,46 @@
+import re
+
+#
+# m = re.match(r"(?P<method>([A-Z]]+)) (?P<url>\/(.*)\s+) (?P<version>[A-Z]+\/(\d+)\.(\d+))", "GET /index.php?option=com_content&view=article&id=46&Itemid=54 HTTP/1.1")
+s = '194.232.72.121 - - [24/Jul/2017:16:24:42 +0200] "GET /images/phocagallery/almhuette/thumbs/phoca_thumb_l_almhuette_raith_007.jpg HTTP/1.1" 200 87330 "http://www.almhuette-raith.at/index.php?option=com_phocagallery&view=category&id=1&Itemid=53" "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:54.0) Gecko/20100101 Firefox/54.0" "-"'
+# m1 = re.match(r"([A-Z]]+)\s\/(.+?)\s(\/.+?)", "GET /index.php?option=com_content&view=article&id=46&Itemid=54 HTTP/1.1")
+# m2 = re.search(r'\s\/(.*)\s+', "GET /index.php?option=com_content&view=article&id=46&Itemid=54 HTTP/1.1")
+# m3 = re.match(r"([A-Z]+)", "GET /index.php?option=com_content&view=article&id=46&Itemid=54 HTTP/1.1")
+# m4 = re.search(r'([A-Z]+)\s+\/(.+?)\s', "GET /index.php?option=com_content&view=article&id=46&Itemid=54 HTTP/1.1")
+# m5 = re.search(r'[A-Z]+\/(\d+)\.(\d+)', s)
+# m5 = re.search(r'([^\s]+)', s)
+# m6 = re.search(r'\[([^\]]+)\]', s)
+m = re.search(r'[1-9]{1,3}\.[1-9]{1,3}', s)
+m = re.search(r'[0-9]{1,3}(\.[0-9]{1,3}){3}', s)
+
+# lexeme types
+WSP, QUOTED_STRING, DATE, RAW, NO_DATA = range(5) # ENUM
+
+RULES = [
+    ('\s+', WSP),
+    ('-|"-"', NO_DATA),
+    ('"([^"]+)"', QUOTED_STRING),
+    ('\[([^\]]+)\]', DATE),
+    ('([^\s]+)', RAW),
+    ]
+
+
+def Lexer(rules):
+    # предварительно компилируем регулярные выражения для ускорения работы
+    prepared = [(re.compile(regexp), token_type) for regexp, token_type in rules]
+
+    def lex(line):
+        ll = len(line) # длина строки лога - чтобы знать, когда остановиться
+        i = 0          # текущая позиция анализатора
+        while i < ll:
+            for pattern, token_type in prepared:  # пробуем регулярные выражения по очереди
+                match = pattern.match(line, i)    # проверяем соответствует ли регулярное выражение строке с позиции i
+                if match is None:                 # если нет - пробуем следующую регулярку
+                    continue
+                i = match.end()                   # передвигаем позицию анализатора до индекса, соответствующего концу совпадения
+                yield (match, token_type)         # возвращаем найденный токен
+                break                             # начинаем анализировать остаток строки с новым значением сдвига i
+            # по хорошему, в этом месте нужно кидать ошибку SyntaxError(line, i) в случае, если ни один из шаблонов не совпал
+    return lex
+
+lexer = Lexer(RULES)
